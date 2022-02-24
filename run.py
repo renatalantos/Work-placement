a97b78b9d4bc4e51163d58211970907bc8eb1cf7
renatalantos <renatalantos@gmail.com>
2022-02-24 10:26:44 +0000

    Created separate functions for different commands

--- run.py
+++ run.py
@@ -1,44 +1,57 @@
 import subprocess
-import os
+"""
+We use Python subprocess module to execute system commands.
+"""
+
 
 def create_dir():
+    """
+    Creates a directory called experiment in the root directory.
+    """
     cmd = "mkdir experiment"
     returned_value = subprocess.call(cmd, shell=True)
+    print("1")
 create_dir()
-
-# def create_file():
-#     cmd = "touch hello.txt"
-#     returned_value = subprocess.call(cmd, shell=True)
-# create_file()
 
 
 def create_file_and_populate():
+    """
+    Creates a file called hello.txt in root directory and populates it
+    with print("Hello World") statement. Closes file after poulating.
+    No use of subprocess module here.
+    """
     cmd = open("hello.txt", "w")
     cmd.write('print("Hello World")')
     cmd.close()
+    print("2")
 create_file_and_populate()
 
+
 def move_file_into_dir():
+    """
+    Moves file hello.txt into experiment directory.
+    """
     cmd = "mv hello.txt experiment"
     returned_value = subprocess.call(cmd, shell=True)
+    print("3")
 move_file_into_dir()
 
-# def create_file_and_populate():
-#     cmd = open("hello.txt", "w")
-#     cmd.write('print("Hello World")')
-#     cmd.close()
-# create_file_and_populate()
-# This is where the process stops and there are errors
-# Also, another hello.txt file is created, with the print("Hello, World") command in it
-# There is nothing in the hello.txt file in the experiment directory
-# returned_value = subprocess.call(cmd4, shell=True)
 
 def run_file():
+    """
+    Executes content of hello.txt file.
+    """
     cmd = "python3 experiment/hello.txt"
     returned_value = subprocess.call(cmd, shell=True)
+    print("4")
 run_file()
 
+
 def delete_file():
+    """
+    Deletes hello.txt from experiment directory.
+    """
     cmd = "rm experiment/hello.txt"
     returned_value = subprocess.call(cmd, shell=True)
+    print("5")
 delete_file()
